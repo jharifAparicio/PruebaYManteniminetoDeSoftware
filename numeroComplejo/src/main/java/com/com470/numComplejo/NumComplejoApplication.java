f5dbd69d1555073c057a9dd0a05c9dfdbd560f7a
Juan Carlos Bergman Villarroel <juancarlos.bergman@gmail.com>
2025-09-02 14:16:49 -0400

    test

---
+++ numeroComplejo/src/main/java/com/com470/numComplejo/NumComplejoApplication.java
@@ -0,0 +1,13 @@
+package com.com470.numComplejo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumComplejoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumComplejoApplication.class, args);
+	}
+
+}
---
+++ numeroComplejo/src/test/java/com/com470/numComplejo/NumComplejoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com470.numComplejo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumComplejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
